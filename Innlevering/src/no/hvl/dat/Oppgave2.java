dd174a887597f9b4a5693d0d55f9d5cb09523871
ramlawarsame <ramlawarsame@10.0.0.5>
2021-09-10 01:44:12 +0200

    innlevering 1

---
+++ Innlevering/src/no/hvl/dat/Oppgave2.java
@@ -0,0 +1,15 @@
+package no.hvl.dat;
+
+public class Oppgave2 {
+	
+		public static void main(String[] args) {
+			int i = 20; 
+			while (i >= 1) {
+				System.out.println(i); 
+				i--; 
+			}
+			
+		}
+
+
+}
---
+++ Innlevering/src/no/hvl/dat/Oppgave3.java
@@ -0,0 +1,34 @@
+package no.hvl.dat;
+
+public class Oppgave3 {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 20; i++) {
+			switch (i) {
+			case 1:
+				System.out.println("A  ");
+				break;
+			case 2:
+				System.out.println("B  ");
+				break;
+			case 3:
+				System.out.println("C  ");
+				break;
+
+			}
+			System.out.println(i);
+
+			if (i == 1) {
+				System.out.println("A");
+			} else if (i == 2) {
+				System.out.println("B");
+
+			} else {
+				System.out.println("C");
+
+			}
+
+		}
+
+	}
+
+}
