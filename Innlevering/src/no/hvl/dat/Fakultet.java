b648bf7e5b679fb1271801a4ee85ff31ad349d89
ramlawarsame <ramlawarsame@10.0.0.5>
2021-09-10 21:32:30 +0200

    Innlevering 1

---
+++ Innlevering/src/no/hvl/dat/Fakultet.java
@@ -0,0 +1,26 @@
+package no.hvl.dat;
+
+import java.util.Scanner;
+
+public class Fakultet {
+
+	public static void main(String[] args) {
+		// lage et program som leser inn et heltall n>0, deretter
+		// beregner verdien n!.
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println(" Skriv et heltall: ");
+
+		int nr = scanner.nextInt();
+		int i, fact = 1;
+
+		for (i = 1; i <= nr; i++) {
+
+			fact = fact * i;
+		}
+		System.out.println("Fakultet av " + nr + " er: " + fact);
+
+	}
+
+}
